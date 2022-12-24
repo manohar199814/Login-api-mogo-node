ec27597066634014294b424b3ccb3acbd08593e1
manohar <manoharmanu2614@gmail.com>
2022-12-24 11:04:39 +0530

    add audit route and logs

---
+++ simple-login-api-node-mongo-master/users/user.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+    username: { type: String, unique: true, required: true },
+    hash: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    createdDate: { type: Date, default: Date.now },
+    Logs:[{
+        clientIP:String,
+        logInTime:Date
+    }],
+    role:String
+});
+
+schema.set('toJSON', { virtuals: true });
+
+module.exports = mongoose.model('User', schema);
---
+++ users/user.service.js
@@ -0,0 +1,98 @@
+﻿const config = require('config.js');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const db = require('_helpers/db');
+const User = db.User;
+
+module.exports = {
+    authenticate,
+    getAll,
+    getById,
+    create,
+    update,
+    delete: _delete,
+    audit
+};
+
+async function authenticate({ username, password , ip }) {
+    const user = await User.findOne({ username });
+    if (user && bcrypt.compareSync(password, user.hash)) {
+        const { hash, ...userWithoutHash } = user.toObject();
+        const token = jwt.sign({ sub: user.id }, config.secret);
+        //add logs to user i.e. ip address and time stamp
+        let log = {
+            clientIP:ip,
+            logInTime:new Date()
+        }
+        user.Logs.push(log);
+        user.save();
+        return {
+            ...userWithoutHash,
+            token
+        };
+    }
+
+    
+}
+
+async function audit(id){
+    //find user by id
+    const user = await User.findById(id);
+    //check if user has role property 
+    //1) if no role property return null
+    //2) if role exists and role is Auditor send logs
+    if(!user.role || (user.role && user.role.toUpperCase() !== 'AUDITOR')){
+        return null;
+    }else{
+        return await User.find().select({Logs:1});
+    }
+}
+
+async function getAll() {
+    return await User.find().select('-hash');
+}
+
+async function getById(id) {
+    return await User.findById(id).select('-hash');
+}
+
+async function create(userParam) {
+    // validate
+    if (await User.findOne({ username: userParam.username })) {
+        throw 'Username "' + userParam.username + '" is already taken';
+    }
+
+    const user = new User(userParam);
+
+    // hash password
+    if (userParam.password) {
+        user.hash = bcrypt.hashSync(userParam.password, 10);
+    }
+
+    // save user
+    await user.save();
+}
+
+async function update(id, userParam) {
+    const user = await User.findById(id);
+
+    // validate
+    if (!user) throw 'User not found';
+    if (user.username !== userParam.username && await User.findOne({ username: userParam.username })) {
+        throw 'Username "' + userParam.username + '" is already taken';
+    }
+
+    // hash password if it was entered
+    if (userParam.password) {
+        userParam.hash = bcrypt.hashSync(userParam.password, 10);
+    }
+
+    // copy userParam properties to user
+    Object.assign(user, userParam);
+
+    await user.save();
+}
+
+async function _delete(id) {
+    await User.findByIdAndRemove(id);
+}
